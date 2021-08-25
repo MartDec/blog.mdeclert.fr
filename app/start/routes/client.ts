01831c117b69db44e9772d9bc1bd75f9ab43012b
Martin Déclert <mdeclert.dev@gmail.com>
2021-08-25 17:55:34 +0200

    Client posts page created

--- app/start/routes/client.ts
+++ app/start/routes/client.ts
@@ -1,6 +1,7 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', 'PageController.render')
+Route.get('/posts', 'PostController.getPosts')
 Route.get('/login', 'PageController.render').middleware('guest')
 Route.get('/me', 'PageController.render').middleware('auth:web')
 
